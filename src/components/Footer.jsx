45dfc7e6cfa8a11d28eea75a391c2c3a48d854e8
Cihanser <cihansercaliskan@gmail.com>
2025-10-17 12:41:38 +0300

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,14 +15,6 @@ const Footer = () => {
           </div>
           
           <div className="footer-section">
-            <h4>Quick Links</h4>
-            <ul className="footer-links">
-              <li><a href="#about">About</a></li>
-              <li><a href="#contact">Contact</a></li>
-            </ul>
-          </div>
-          
-          <div className="footer-section">
             <h4>Contact</h4>
             <ul className="contact-info">
               <li>
